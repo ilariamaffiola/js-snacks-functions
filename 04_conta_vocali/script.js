d79d604e76b24648e0a2f6aac4a41115e81b5ccf
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-24 16:15:38 +0100

    [UPD]Add function to count vowels in a string and log the result

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,27 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVocali(string){
+    //metto una variabile che mi conta le vocali
+    let vocali = 0;
+    //inizializzo un ciclo for per scorrere la stringa
+    for (let i=0; i<string.length; i++){
+        //controllo se il tutto funziona con un console.log
+        console.log(string[i]);
+        //metto un if che controlla se la lettera è una vocale e se si incrementa la variabile vocali
+        if(string[i] === 'a' || string[i] === 'e' || string[i] === 'i' || string[i] === 'o' || string[i] === 'u'){
+            vocali= vocali + 1;
+        }
+    }
+    //ritorno il numero di vocali
+    return vocali;
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+let vocali = countVocali(word);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+console.log(vocali);
