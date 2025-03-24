09435a8ede84af29e153141d6c9052107ed01ca6
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-24 16:40:31 +0100

    [UPD]ESERCIZIO FINITO + PRIMO BONUS

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,29 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+// const d = new Date();
+// console.log  (d.getHours());
+function saluto(string){
+    //dichiaro una variabile che mi dia l'ora attuale
+    let d = new Date();
+    //controllo se il tutto funziona con un console.log
+    console.log(d.getHours());
+    //inizialized un if che controlla l'ora e restituisce il saluto corretto
+    if(d.getHours() <= 13){
+        return 'Buongiorno' + ' ' + string;
+    }
+    else if(d.getHours() >13 && d.getHours() <= 17){
+        return 'Buon Pomeriggio' + ' ' + string;
+    }
+    else{
+        return 'Buonasera' + ' ' + string;
+    }
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let salutoUtente = saluto(name);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+console.log(salutoUtente);
