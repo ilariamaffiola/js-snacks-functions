e3c03e977cb2bc51743a6388de35df613946be1c
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-24 16:00:29 +0100

    [UPD]Implement function to filter names by initial letter and log results

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function filterNames(names , letters){
+    let filteredNames = [];
+    for(i=0; i<names.length; i++){
+        //controllo se il tutto funziona con un console.log e nel let fuori dalla funzione inizialmente metto solo la condizione names
+        console.log(names[i]);
+        //propongo un if dove si va a prendere la prima lettera di ogni nome e si confronta con la lettera inserita dall'utente per poi pusharla nell'array vuoto
+        if(names[i].charAt(0) === letters){
+            filteredNames.push(names[i]);
+        }
+    }
+    //ritorno l'array filtrato
+    return filteredNames;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+let arrayFiltrato = filterNames(names , 'A');
 
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+console.log(arrayFiltrato);
